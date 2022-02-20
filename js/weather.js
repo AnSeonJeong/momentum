46047e923a91a2400e7f586c29569bc87e9d812c
AnSeonJeong <98884055+AnSeonJeong@users.noreply.github.com>
2022-02-21 05:24:04 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     console.log(`현재 '${lat} ${lon}'에 있습니다.`);
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
 
     
     fetch(url)
